26a3b80619a30b1a28cc581e9b8a96a1152ad129
Valinora <cjamison@valmagus.com>
2025-03-07 15:19:24 -0800

    Output a UNIX timestamp

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, tx) => {
-    time = new Date().toISOString();
+    time = new Date().getTime();
+    // Convert from millisecond precision to second precision
+    time = Math.floor(time / 1000);
     response = {"message": "My name is Colin Jamison", "timestamp": time};
     tx.type('json');
     tx.send(JSON.stringify(response));
