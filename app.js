4f936e9d99349951ec61690665484cf3c01eb448
Valinora <cjamison@valmagus.com>
2025-03-07 14:46:24 -0800

    Yarn initialization, express hello-world

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, tx) => {
+    time = new Date().toISOString();
+    response = {"message": "My name is Colin Jamison", "timestamp": time};
+    tx.type('json');
+    tx.send(JSON.stringify(response));
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
