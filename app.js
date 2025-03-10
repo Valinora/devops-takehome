650cb2c1fcf0b272b76d81de6fc6169283f1eae7
Valinora <cjamison@valmagus.com>
2025-03-10 16:37:53 -0700

    Stop dropping precision.

--- app.js
+++ app.js
@@ -4,8 +4,6 @@ const port = 80;
 
 app.get('/', (req, tx) => {
     time = new Date().getTime();
-    // Convert from millisecond precision to second precision
-    time = Math.floor(time / 1000);
     response = {"message": "My name is Colin Jamison", "timestamp": time};
     tx.type('json');
     tx.send(JSON.stringify(response));
